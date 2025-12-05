f317e8023195f20077352bfe02749612bcfcecb1
JimJos-Calderon <jimjos.calderon@gmail.com>
2025-12-05 09:18:50 +0100

    to_lower

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 def multiplicacion(a,b):
     return a * b
+
+def to_upper(text):
+    return text.upper()
+
+def to_lower(text):
+    return text.upper()
     
--- test/test_main.py
+++ test/test_main.py
@@ -1,3 +1,10 @@
-from src.main import multiplicacion
+from src.main import multiplicacion, to_lower, to_upper
 def test_multiplicacion():
     assert multiplicacion(2,3)==6
+    
+def test_to_upper():
+    assert to_upper("jimjos")=="JIMJOS"
+    
+
+def test_to_lower():
+    assert to_lower('JIMJOS')=='jimjos'
